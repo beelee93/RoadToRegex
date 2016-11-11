dfbdbc7787f1395273cb4e14d4e2cd8819d1bd2e
beelee93 <beelee93@gmail.com>
2016-11-12 03:16:02 +0800

    Updated example 1

--- src/regex1/Example.java
+++ src/regex1/Example.java
@@ -9,21 +9,30 @@ import java.util.Scanner;
 public class Example {
     public static void main(String[] args) {
         DeterministicFA fa = new DeterministicFA();
+        
+        // create the nodes
         DFANode node0 = fa.createNode(false),
-                node1 = fa.createNode(true),
-                node2 = fa.createNode(false);
+                node1 = fa.createNode(true);
         
-        node0.set('a', node1);
-        node1.set('b', node2);
-        node2.set('a', node1);
+        // Node0 -> Node1 on [1-9]
+        for(int i=1;i<10;i++) 
+            node0.set((char)(0x30+i), node1);
         
+        // Node1 -> Node1 on [0-9]
+        for(int i=0;i<10;i++) 
+            node1.set((char)(0x30+i), node1);
+        
+        // Node1 -> Node0 on + or -
+        node1.set('+', node0);
+        node1.set('-', node0);
+        
+        // read input and verify
         Scanner sc = new Scanner(System.in);
         
-        System.out.print("String to test against 'a(ba)*' regex: ");
+        System.out.print("String to test against '[1-9][0-9]*([\\+-][1-9][0-9]*)*' regex: ");
         String line = sc.nextLine().trim();
         
-        int i;
-        for(i=0;i<line.length();i++) {
+        for(int i=0;i<line.length();i++) {
             if(!fa.nextInput(line.charAt(i)))
                 break;
         }
